d556703329c93231ebd4f6dee85ae8a730ba1e6b
pema9 <lama7ghale@gmail.com>
2022-06-24 08:17:59 +0545

    first

---
+++ WasteManagement System/Controllers/WasteController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WasteManagement_System.Controllers
+{
+    public class WasteController : Controller
+    {
+        public ViewResult Signup()
+        {
+            return View();
+        }
+        public ViewResult Login()
+        {
+            return View();
+        }
+        public ViewResult Contactus()
+        {
+            return View();
+        }
+        public ViewResult Aboutus()
+        {
+            return View();
+        }
+        public ViewResult Services()
+        {
+            return View();
+        }
+
+    }
+}
